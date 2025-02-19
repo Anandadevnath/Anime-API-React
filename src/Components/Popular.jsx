ac5f264e84b7c3aab66884717add0977f26fbc1c
Ananda Dev Nath <thunderhorizon18@gmail.com>
2025-02-19 15:02:17 +0600

    Changed to vite

---
+++ src/Components/Popular.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import '../index.css'
+import Sidebar from './Sidebar';
+import { useGlobalContext } from '../context/global';
+
+function Popular({ rendered }) {
+
+  const { popularAnime, isSearch, searchResults } = useGlobalContext()
+
+  const conditionalRender = () => {
+    if (!isSearch && rendered === 'popular') {
+      return popularAnime?.map((anime) => {
+        return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+          <img src={anime.images.jpg.large_image_url} alt="" />
+          <p>{anime.title}</p>
+        </Link>
+      })
+    } else {
+      return searchResults?.map((anime) => {
+        return <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+          <img src={anime.images.jpg.large_image_url} alt="" />
+          <p>{anime.title}</p>
+        </Link>
+      })
+    }
+  }
+
+  return (
+    <div>
+       <Sidebar />
+      <div className='popular-anime'>
+        {conditionalRender()}
+      </div>
+       
+    </div>
+  )
+}
+
+export default Popular
---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import Popular from '../Components/Popular';
+import Airing from '../Components/Airing';
+import Upcoming from '../Components/Upcoming';
+import { useGlobalContext } from '../context/global';
+
+function Homepage() {
+
+    const [rendered, setRendered] = useState('popular')
+
+    const {
+        handleSubmit,
+        search,
+        searchAnime,
+        handleChange,
+        getUpcomingAnime,
+        getAiringAnime,
+        getPopularAnime,
+    } = useGlobalContext()
+
+    const switchComponent = () => {
+        switch (rendered) {
+            case 'popular':
+                return <Popular rendered={rendered} />
+            case 'airing':
+                return <Airing rendered={rendered} />
+            case 'upcoming':
+                return <Upcoming rendered={rendered} />
+            default:
+                return <Popular rendered={rendered} />
+        }
+    }
+
+    return (
+        <div>
+            <header>
+                <div className="logo">
+                    <h1>
+                        {rendered === 'popular' ? 'Popular Anime 🔥' :
+                            rendered === 'airing' ? 'Airing Anime 📺' : 'Upcoming Anime 📅'}
+                    </h1>
+                </div>
+                <div className="search-container">
+                    <div className="filter-btn popular-filter">
+                        <button onClick={() => {
+                            setRendered('popular')
+                        }}>Popular <i className="fas fa-fire"></i></button>
+                    </div>
+                    <form action="" className="search-form" onSubmit={handleSubmit}>
+                        <div className="input-control">
+                            <input type="text" placeholder="Search Anime" value={search} onChange={handleChange} />
+                            <button type="submit">Search</button>
+                        </div>
+                    </form>
+                    <div className="filter-btn airing-filter">
+                        <button onClick={() => {
+                            setRendered('airing')
+                            getAiringAnime()
+                        }}>Airing</button>
+                    </div>
+                    <div className="filter-btn upcoming-filter">
+                        <button onClick={() => {
+                            setRendered('upcoming')
+                            getUpcomingAnime()
+                        }}>Upcoming</button>
+                    </div>
+                </div>
+            </header>
+            {switchComponent()}
+        </div >
+    )
+}
+
+export default Homepage
